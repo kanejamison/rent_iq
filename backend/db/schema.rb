5e48e4695db9b4c67476aab9822b84a806685189
Kane <918780+kanejamison@users.noreply.github.com>
2025-10-20 13:16:22 -0700

    rails generate scaffold Property name:string address:text year_built:integer website_url:string

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_20_201602) do
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.integer "year_built"
+    t.string "website_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
